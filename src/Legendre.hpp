96198c2f48f393f64cc207b2b8e383828ea8261a
Sean McBane <sean.mcbane@protonmail.com>
2021-05-12 18:27:46 -0500

    Legendre polynomials work correctly

--- src/Legendre.hpp
+++ src/Legendre.hpp
@@ -1,7 +1,8 @@
 #ifndef LEGENDRE_HPP
 #define LEGENDRE_HPP
 
-#include "Metanomials.hpp"
+#include "Polynomial.hpp"
+#include "Rationals.hpp"
 
 #include <array>
 #include <limits>
@@ -15,24 +16,19 @@ namespace Legendre
 // This works up to 14th order before hitting a constexpr evaluation limit in
 // clang.
 template <auto I>
-constexpr inline auto polynomial = 
-    (Metanomials::metanomial(
-        term(Rationals::rational<2*I-1>,
-             Metanomials::powers(intgr_constant<1>))) * polynomial<I-1>
-    - polynomial<I-2> * intgr_constant<I-1>) /
-    intgr_constant<I>;
+constexpr inline auto polynomial = (Polynomials::make_poly(
+                                        std::tuple(2*I-1),
+                                        Polynomials::PowersList<Polynomials::Powers<1>>{}) *
+                                        polynomial<I - 1> +
+                                    polynomial<I - 2> * (1 - I)) / static_cast<double>(I);
 
 template <>
-constexpr inline auto polynomial<0> = Metanomials::metanomial(
-    term(Rationals::rational<1>,
-         Metanomials::powers(intgr_constant<0>))
-);
+constexpr inline auto polynomial<0> = Polynomials::make_poly(
+    std::tuple(1), Polynomials::PowersList<Polynomials::Powers<0>>{});
 
 template <>
-constexpr inline auto polynomial<1> = Metanomials::metanomial(
-    term(Rationals::rational<1>,
-         Metanomials::powers(intgr_constant<1>))
-);
+constexpr inline auto polynomial<1> = Polynomials::make_poly(
+    std::tuple(1), Polynomials::PowersList<Polynomials::Powers<1>>{});
 
 /********************************************************************************
  * Test that Legendre polynomials are correct.
@@ -40,31 +36,49 @@ constexpr inline auto polynomial<1> = Metanomials::metanomial(
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
 
-using namespace Metanomials;
+using namespace Polynomials;
 using namespace Rationals;
 
 TEST_CASE("Test computed Legendre polynomials")
 {
-    REQUIRE(polynomial<0> == Metanomials::metanomial(term(rational<1>, powers(intgr_constant<0>))));
-    REQUIRE(polynomial<1> == Metanomials::metanomial(term(rational<1>, powers(intgr_constant<1>))));
-    REQUIRE(polynomial<2> ==
-        Metanomials::metanomial(
-            term(rational<3, 2>, powers(intgr_constant<2>)),
-            term(-rational<1, 2>, powers(intgr_constant<0>))
-        )
-    );
+    SUBCASE("Order 0")
+    {
+        constexpr auto poly = polynomial<0>;
+        REQUIRE(poly.coeffs()[0] == 1.0);
+        static_assert(std::is_same_v<std::decay_t<decltype(poly)>, Polynomial<int, Powers<0>>>);
+    }
 
-    REQUIRE(polynomial<10> ==
-        metanomial(
-            term(rational<46189, 256>, powers(intgr_constant<10>)),
-            term(-rational<109395, 256>, powers(intgr_constant<8>)),
-            term(rational<90090, 256>, powers(intgr_constant<6>)),
-            term(-rational<30030, 256>, powers(intgr_constant<4>)),
-            term(rational<3465, 256>, powers(intgr_constant<2>)),
-            term(-rational<63, 256>, powers(intgr_constant<0>))
-        )
-    );
-}
+    SUBCASE("Order 1")
+    {
+        constexpr auto poly = polynomial<1>;
+        REQUIRE(poly.coeffs()[0] == 1.0);
+        static_assert(std::is_same_v<std::decay_t<decltype(poly)>, Polynomial<int, Powers<1>>>);
+    }
+
+    SUBCASE("Order 2")
+    {
+        constexpr auto poly = polynomial<2>;
+        REQUIRE(poly.coeffs()[0] == -0.5);
+        REQUIRE(poly.coeffs()[1] == 1.5);
+        static_assert(
+            std::is_same_v<std::decay_t<decltype(poly)>, Polynomial<double, Powers<0>, Powers<2>>>);
+    }
+
+    SUBCASE("Order 10")
+    {
+        constexpr auto poly = polynomial<10>;
+        REQUIRE(poly.coeffs()[0] == doctest::Approx(-63.0 / 256));
+        REQUIRE(poly.coeffs()[1] == doctest::Approx(3465.0 / 256));
+        REQUIRE(poly.coeffs()[2] == doctest::Approx(-30030.0 / 256));
+        REQUIRE(poly.coeffs()[3] == doctest::Approx(90090.0 / 256));
+        REQUIRE(poly.coeffs()[4] == doctest::Approx(-109395.0 / 256));
+        REQUIRE(poly.coeffs()[5] == doctest::Approx(46189.0 / 256));
+        static_assert(
+            std::is_same_v<
+                std::decay_t<decltype(poly)>,
+                Polynomial<double, Powers<0>, Powers<2>, Powers<4>, Powers<6>, Powers<8>, Powers<10>>>);
+    }
+} // TEST_CASE
 
 #endif /* DOCTEST_LIBRARY_INCLUDED */
 
@@ -74,9 +88,9 @@ TEST_CASE("Test computed Legendre polynomials")
 template <class T, class P>
 constexpr auto interval_root(P poly, T low, T high)
 {
-    constexpr auto pprime = P::template partial<0>();
+    auto pprime = poly.template partial<0>();
     T mid = (high + low) / 2;
-    constexpr auto abs = [] (auto x) { return x < 0 ? -x : x; };
+    constexpr auto abs = [](auto x) { return x < 0 ? -x : x; };
     while (poly(mid) != zero<T>)
     {
         auto delta = -poly(mid) / pprime(mid);
@@ -94,18 +108,18 @@ constexpr auto all_roots()
 {
     if constexpr (Order == 1)
     {
-        return std::array<T, 1> { 0 };
+        return std::array<T, 1>{0};
     }
     else
     {
-        auto extrema = all_roots<T, Order-1>();
-        std::array<T, Order> my_roots { 0 };
+        auto extrema = all_roots<T, Order - 1>();
+        std::array<T, Order> my_roots{0};
         my_roots[0] = interval_root(polynomial<Order>, -one<T>, extrema[0]);
-        for (int i = 1; i < Order-1; ++i)
+        for (int i = 1; i < Order - 1; ++i)
         {
-            my_roots[i] = interval_root(polynomial<Order>, extrema[i-1], extrema[i]);
+            my_roots[i] = interval_root(polynomial<Order>, extrema[i - 1], extrema[i]);
         }
-        my_roots[Order-1] = interval_root(polynomial<Order>, extrema[Order-2], one<T>);
+        my_roots[Order - 1] = interval_root(polynomial<Order>, extrema[Order - 2], one<T>);
         return my_roots;
     }
 }
@@ -134,22 +148,14 @@ TEST_CASE("Find roots of Legendre polynomials")
     REQUIRE(roots<double, 3>[1] == doctest::Approx(0.0));
     REQUIRE(roots<double, 3>[2] == doctest::Approx(std::sqrt(3.0 / 5)));
 
-    REQUIRE(roots<double, 5>[0] == doctest::Approx(
-        -std::sqrt(5 + 2 * std::sqrt(10.0 / 7)) / 3
-    ));
-    REQUIRE(roots<double, 5>[1] == doctest::Approx(
-        -std::sqrt(5 - 2 * std::sqrt(10.0 / 7)) / 3
-    ));
+    REQUIRE(roots<double, 5>[0] == doctest::Approx(-std::sqrt(5 + 2 * std::sqrt(10.0 / 7)) / 3));
+    REQUIRE(roots<double, 5>[1] == doctest::Approx(-std::sqrt(5 - 2 * std::sqrt(10.0 / 7)) / 3));
     REQUIRE(roots<double, 5>[2] == doctest::Approx(0.0));
-    REQUIRE(roots<double, 5>[3] == doctest::Approx(
-        std::sqrt(5 - 2 * std::sqrt(10.0 / 7)) / 3
-    ));
-    REQUIRE(roots<double, 5>[4] == doctest::Approx(
-        std::sqrt(5 + 2 * std::sqrt(10.0 / 7)) / 3
-    ));
+    REQUIRE(roots<double, 5>[3] == doctest::Approx(std::sqrt(5 - 2 * std::sqrt(10.0 / 7)) / 3));
+    REQUIRE(roots<double, 5>[4] == doctest::Approx(std::sqrt(5 + 2 * std::sqrt(10.0 / 7)) / 3));
 }
 
-#endif /* DOCTEST_LIBRARY_INCLUDED */
+#endif /* DOCTEST_LIBRARY_INCLUDED *
 
 /********************************************************************************
  *******************************************************************************/
