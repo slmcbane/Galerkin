63f0549e51661090264e2b829bdf99ceaf281cc8
Sean McBane <sean.mcbane@protonmail.com>
2021-05-13 15:06:35 -0500

    Update Rational to be a single type

--- src/Rationals.hpp
+++ src/Rationals.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Sean McBane
+ * Copyright (c) 2021, Sean McBane and The University of Texas at Austin.
  * This file is part of the Galerkin library; Galerkin is copyright under the
  * terms of the MIT license. Please see the top-level COPYRIGHT file for details.
  */
@@ -30,59 +30,24 @@ namespace Rationals
  * could be made a int128_t, but in practice compiler limits will be reached
  * before overflow, anyway. Should be a signed type.
  */
-typedef int64_t rational_num_t;
+typedef int64_t rational_num_type;
 
 /// The type used for the denominator in a rational. Should be an unsigned type.
-typedef uint64_t rational_den_t;
-
-/*!
- * @brief Type representing a rational number at compile time.
- * 
- * `Rational` is used to do exact calculations of derivatives, Jacobians, etc.
- * by manipulating rational numbers at compile time. The normal arithmetic
- * operators are all defined for it, and it decays to a `double` as appropriate.
- * Rather than use this class template, however, you should use the `rational`
- * template constant from this header; instantiating `rational<N, D>`
- * automatically reduces the resulting fraction to its simplest form.
- */
-template <rational_num_t Num, rational_den_t Den>
-struct Rational
-{
-    static_assert(Den != zero<rational_den_t>);
-
-    /// Get the numerator
-    static constexpr auto num() { return Num; }
-    /// Get the denominator
-    static constexpr auto den() { return Den; }
-
-    /*! @brief Convert the number to a double precision float, for when you have to
-     * do inexact floating point operations or run-time computation :(.
-     */
-    constexpr operator double() const
-    {
-        return static_cast<double>(Num) / Den;
-    }
-};
-
-template <class T>
-constexpr bool is_rational = false;
-
-template <rational_num_t N, rational_den_t D>
-constexpr bool is_rational<Rational<N, D>> = true;
+typedef uint64_t rational_den_type;
 
 /*!
  * @brief Utility; find greatest common denominator of `a` and `b`. This will hit
  * `constexpr` evaluation limits when `a` or `b` becomes large relative to the
  * other.
  */
-constexpr auto gcd(rational_num_t a, rational_den_t b)
+constexpr auto gcd(rational_num_type a, rational_den_type b)
 {
     if (a < 0)
     {
         a = -a;
     }
 
-    auto x = static_cast<rational_den_t>(a);
+    auto x = static_cast<rational_den_type>(a);
     auto y = x > b ? b : x;
     x = x > b ? x : b;
 
@@ -105,30 +70,84 @@ constexpr auto gcd(rational_num_t a, rational_den_t b)
     return x;
 }
 
-/// Reduce a rational number to its simplest representation.
-template <rational_num_t N, rational_den_t D>
-constexpr auto reduce_rational(Rational<N, D>)
+namespace detail
 {
-    constexpr auto div = gcd(N, D);
-    return Rational<N / static_cast<rational_num_t>(div), D / div>();
+
+/// Reduce a rational number to its simplest representation.
+constexpr auto reduce_rational(rational_num_type n, rational_den_type d)
+{
+    auto div = gcd(n, d);
+    return std::tuple(n / static_cast<rational_num_type>(div), d / div);
 }
+
+} // namespace detail
+
+/*!
+ * @brief Type representing a rational number at compile time.
+ *
+ * `Rational` is used to do exact calculations of derivatives, Jacobians, etc.
+ * by manipulating rational numbers at compile time. The normal arithmetic
+ * operators are all defined for it, and it decays to a `double` as appropriate.
+ * Rather than use this class template, however, you should use the `rational`
+ * template constant from this header; instantiating `rational<N, D>`
+ * automatically reduces the resulting fraction to its simplest form.
+ */
+class Rational
+{
+  public:
+    /// Get the numerator
+    constexpr auto num() const noexcept { return m_num; }
+    /// Get the denominator
+    constexpr auto den() const noexcept { return m_den; }
+
+    /*! @brief Convert the number to a double precision float, for when you have to
+     * do inexact floating point operations or run-time computation :(.
+     */
+    constexpr operator double() const noexcept { return static_cast<double>(m_num) / m_den; }
+
+    template <class T, class = std::enable_if_t<std::is_integral_v<T>>>
+    constexpr Rational(T x) : m_num(x), m_den{1}
+    {
+    }
+
+    template <
+        class T1, class T2, class = std::enable_if_t<std::is_integral_v<T1> && std::is_integral_v<T2>>>
+    constexpr Rational(T1 x, T2 y) : m_num(x), m_den{1}
+    {
+        if (y < 0)
+        {
+            m_num *= -1;
+            m_den = -y;
+        }
+        else
+        {
+            m_den = y;
+        }
+        auto reduced = detail::reduce_rational(m_num, m_den);
+        m_num = std::get<0>(reduced);
+        m_den = std::get<1>(reduced);
+    }
+
+  private:
+    rational_num_type m_num;
+    rational_den_type m_den;
+};
 
 /*!
  * @brief Template constant for a compile-time rational.
- * 
+ *
  * This constant evaluates to a `Rational` reduced to its lowest terms. Default
  * denominator is 1, so that an integer can be constructed by `rational<n>`.
- * 
+ *
  * @tparam N The numerator
  * @tparam D The denominator. Default value: 1
  */
-template <rational_num_t N, rational_den_t D = 1>
-constexpr auto rational = reduce_rational(Rational<N, D>());
+template <rational_num_type N, rational_den_type D = 1>
+constexpr auto rational = Rational(N, D);
 
-template <auto N1, auto D1, auto N2, auto D2>
-constexpr bool operator==(Rational<N1, D1>, Rational<N2, D2>)
+constexpr bool operator==(const Rational &x, const Rational &y)
 {
-    return std::is_same_v<decltype(rational<N1, D1>), decltype(rational<N2, D2>)>;
+    return (x.num() == y.num() && x.den() == y.den());
 }
 
 /********************************************************************************
@@ -153,14 +172,15 @@ TEST_CASE("[Galerkin::Rationals] Testing construction of rationals")
 /********************************************************************************
  *******************************************************************************/
 
-template <auto N1, auto D1, auto N2, auto D2>
-constexpr auto operator+(Rational<N1, D1>, Rational<N2, D2>)
+constexpr auto operator+(const Rational &x, const Rational &y)
 {
-    constexpr auto lcm = D1 * D2 / gcd(D1, D2);
-    constexpr auto mult1 = static_cast<rational_num_t>(lcm / D1);
-    constexpr auto mult2 = static_cast<rational_num_t>(lcm / D2);
+    auto lcm = x.den() * y.den() / gcd(x.den(), y.den());
+    auto mult1 = lcm / x.den();
+    auto mult2 = lcm / y.den();
 
-    return rational<N1*mult1 + N2*mult2, D1*mult1>;
+    return Rational(
+        x.num() * static_cast<rational_num_type>(mult1) + y.num() * static_cast<rational_num_type>(mult2),
+        x.den() * mult1);
 }
 
 /********************************************************************************
@@ -183,16 +203,14 @@ TEST_CASE("[Galerkin::Rationals] Testing rational addition")
 /********************************************************************************
  *******************************************************************************/
 
-template <auto N1, auto D1>
-constexpr auto operator-(Rational<N1, D1>)
+constexpr auto operator-(const Rational &x) noexcept
 {
-    return rational<-N1, D1>;
+    return Rational(-x.num(), x.den());
 }
 
-template <auto N1, auto D1, auto N2, auto D2>
-constexpr auto operator-(Rational<N1, D1>, Rational<N2, D2>)
+constexpr auto operator-(const Rational &x, const Rational &y)
 {
-    return Rational<N1, D1>() + (-Rational<N2, D2>());
+    return x + -y;
 }
 
 /********************************************************************************
@@ -215,78 +233,104 @@ TEST_CASE("[Galerkin::Rationals] Testing rational subtraction")
 /********************************************************************************
  *******************************************************************************/
 
-template <auto N1, auto D1, auto N2, auto D2>
-constexpr auto operator*(Rational<N1, D1>, Rational<N2, D2>)
+constexpr auto operator*(const Rational &x, const Rational &y)
 {
-    return rational<N1*N2, D1*D2>;
+    return Rational(x.num() * y.num(), x.den() * y.den());
 }
 
-template <auto N1, auto D1, auto N2, auto D2>
-constexpr auto operator/(Rational<N1, D1>, Rational<N2, D2>)
+constexpr auto operator/(const Rational &x, const Rational &y)
 {
-    constexpr auto num = N1 * static_cast<rational_num_t>(D2);
-    if constexpr (N2 < 0)
+    auto num = x.num() * static_cast<rational_num_type>(y.den());
+    if (y.num() < 0)
     {
-        return -rational<num, D1 * static_cast<rational_den_t>(-N2)>;
+        return Rational(-num, x.den() * static_cast<rational_den_type>(-y.num()));
     }
     else
     {
-        return rational<num, D1 * static_cast<rational_den_t>(N2)>;
+        return Rational(num, x.den() * y.num());
     }
 }
 
 /// A `Rational` * an `integral_constant` returns a `Rational`.
-template <auto N, auto D, class I, I v>
-constexpr auto operator*(Rational<N, D>, std::integral_constant<I, v>) noexcept
+template <class T>
+constexpr std::enable_if_t<std::is_integral_v<T>, Rational>
+operator*(const Rational &x, T y) noexcept
 {
-    return rational<N, D> * rational<v>;
+    return Rational(x.num() * static_cast<rational_num_type>(y), x.den());
 }
 
-template <auto N, auto D, class I, I v>
-constexpr auto operator*(std::integral_constant<I, v>, Rational<N, D>) noexcept
+template <class T>
+constexpr std::enable_if_t<std::is_integral_v<T>, Rational>
+operator*(T y, const Rational &x) noexcept
 {
-    return rational<N, D> * rational<v>;
+    return x * y;
+}
+
+template <class T, T v>
+constexpr Rational operator*(const Rational &x, std::integral_constant<T, v>)
+{
+    return x * v;
+}
+
+template <class T, T v>
+constexpr Rational operator*(std::integral_constant<T, v>, const Rational &x)
+{
+    return x * v;
 }
 
 /// A `Rational` / an `integral_constant` returns a `Rational`.
-template <auto N, auto D, class I, I v>
-constexpr auto operator/(Rational<N, D>, std::integral_constant<I, v>)
+template <class T>
+constexpr std::enable_if_t<std::is_integral_v<T>, Rational>
+operator/(const Rational &x, T y)
 {
-    static_assert(v != 0);
-    if constexpr (v < 0)
+    if (y < 0)
     {
-        return -(rational<N, D> / std::integral_constant<I, -v>());
+        return Rational(-x.num(), x.den() * (-y));
     }
     else
     {
-        return rational<N, D> / rational<v>;
+        return Rational(x.num(), x.den() * y);
     }
 }
 
-template <auto N, auto D, class T>
-constexpr auto operator*(Rational<N, D>, T x)
+template <class T>
+constexpr std::enable_if_t<std::is_integral_v<T>, Rational>
+operator/(T y, const Rational &x)
 {
-    if constexpr (std::is_same_v<T, float>)
+    if (x.num() < 0)
     {
-        return (N * x) / D;
+        return -Rational(y * static_cast<rational_num_type>(x.den()), -x.num());
     }
     else
     {
-        return (N * static_cast<double>(x)) / D;
+        return Rational(y * static_cast<rational_num_type>(x.den()), x.num());
     }
 }
 
-template <auto N, auto D, class T>
-constexpr auto operator/(Rational<N, D>, T x)
+template <class T, T v>
+constexpr Rational operator/(const Rational &x, std::integral_constant<T, v>)
 {
-    if constexpr (std::is_same_v<T, float>)
-    {
-        return N / (D * x);
-    }
-    else
-    {
-        return N / (D * static_cast<double>(x));
-    }
+    return x / v;
+}
+
+template <class T, T v>
+constexpr Rational operator/(std::integral_constant<T, v>, const Rational &x)
+{
+    return v / x;
+}
+
+template <class T>
+constexpr std::enable_if_t<std::is_floating_point_v<T>, double>
+operator*(const Rational &x, T y)
+{
+    return static_cast<double>(x) * y;
+}
+
+template <class T>
+constexpr std::enable_if_t<std::is_floating_point_v<T>, double>
+operator/(const Rational &x, T y)
+{
+    return static_cast<double>(x) / y;
 }
 
 /********************************************************************************
@@ -316,15 +360,13 @@ TEST_CASE("[Galerkin::Rationals] Testing rational multiplication and division")
 
     REQUIRE(rational<1, 2> * 0.5 == doctest::Approx(0.25));
     REQUIRE(rational<1, 2> / 3 == doctest::Approx(1.0 / 6));
-    REQUIRE(std::is_same_v<
-        decltype(rational<1, 2> * std::declval<float>()), float>);
+    REQUIRE(std::is_same_v<decltype(rational<1, 2> * std::declval<float>()), double>);
 }
 
 #endif /* DOCTEST_LIBRARY_INCLUDED */
 
 /********************************************************************************
  *******************************************************************************/
-
 
 } // namespace Rationals
 
