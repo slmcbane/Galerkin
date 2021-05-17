0ca7d9f26da1d6cc8b2eb68025a3b5bd5062bd6e
Sean McBane <sean.mcbane@protonmail.com>
2021-05-17 08:18:25 -0500

    Add tests for Polynomial

--- src/Polynomial.hpp
+++ src/Polynomial.hpp
@@ -337,6 +337,326 @@ constexpr auto partial(const Polynomial<T, Ps...> &p) noexcept
     return p.template partial<I>();
 }
 
+#ifdef DOCTEST_LIBRARY_INCLUDED
+
+TEST_CASE("Add two single-variable polynomials with same powers")
+{
+    constexpr auto coeffs1 = std::tuple(1, 2, 3, 4);
+    constexpr auto coeffs2 = std::tuple(5, 6, 7, 8);
+    constexpr auto powers = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+    constexpr auto poly1 = make_poly(coeffs1, powers);
+    constexpr auto poly2 = make_poly(coeffs2, powers);
+    constexpr auto result = poly1 + poly2;
+
+    REQUIRE(result.num_terms == 4);
+    REQUIRE(result.coeffs()[0] == 6);
+    REQUIRE(result.coeffs()[1] == 8);
+    REQUIRE(result.coeffs()[2] == 10);
+    REQUIRE(result.coeffs()[3] == 12);
+}
+
+TEST_CASE("Add two single-variable polynomials with same powers, in place")
+{
+    constexpr auto coeffs1 = std::tuple(1, 2, 3, 4);
+    constexpr auto coeffs2 = std::tuple(5, 6, 7, 8);
+    constexpr auto powers = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+    auto poly1 = make_poly(coeffs1, powers);
+    constexpr auto poly2 = make_poly(coeffs2, powers);
+    poly1 += poly2;
+
+    REQUIRE(poly1.coeffs()[0] == 6);
+    REQUIRE(poly1.coeffs()[1] == 8);
+    REQUIRE(poly1.coeffs()[2] == 10);
+    REQUIRE(poly1.coeffs()[3] == 12);
+}
+
+TEST_CASE("Add two single-variable polynomials with different powers")
+{
+    constexpr auto coeffs1 = std::tuple(1, 2, 3, 4);
+    constexpr auto coeffs2 = std::tuple(5, 6, 7, 8);
+    constexpr auto powers1 = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+    constexpr auto powers2 = PowersList<Powers<2>, Powers<3>, Powers<4>, Powers<5>>{};
+    constexpr auto poly1 = make_poly(coeffs1, powers1);
+    constexpr auto poly2 = make_poly(coeffs2, powers2);
+    constexpr auto result = poly1 + poly2;
+
+    REQUIRE(result.num_terms == 6);
+    REQUIRE(result.coeffs()[0] == 1);
+    REQUIRE(result.coeffs()[1] == 2);
+    REQUIRE(result.coeffs()[2] == 8);
+    REQUIRE(result.coeffs()[3] == 10);
+    REQUIRE(result.coeffs()[4] == 7);
+    REQUIRE(result.coeffs()[5] == 8);
+}
+
+TEST_CASE("Add two muli-variable polynomials with different powers")
+{
+    constexpr auto coeffs1 = std::tuple(2, -3, 1);
+    constexpr auto coeffs2 = std::tuple(3, 4);
+    constexpr auto powers1 = PowersList<Powers<1, 0>, Powers<1, 1>, Powers<0, 1>>{};
+    constexpr auto powers2 = PowersList<Powers<0, 1>, Powers<2, 0>>{};
+
+    constexpr auto poly1 = make_poly(coeffs1, powers1);
+    constexpr auto poly2 = make_poly(coeffs2, powers2);
+
+    constexpr auto result = poly1 + poly2;
+    REQUIRE(result.num_terms == 4);
+    REQUIRE(result.coeffs()[0] == 4);
+    REQUIRE(result.coeffs()[1] == 2);
+    REQUIRE(result.coeffs()[2] == -3);
+    REQUIRE(result.coeffs()[3] == 4);
+}
+
+TEST_CASE("Construct a polynomial with coefficient array")
+{
+    SUBCASE("Only a single coefficient")
+    {
+        constexpr auto powers = PowersList<Powers<0>>{};
+        constexpr auto coeffs = std::array<double, 1>{1.0};
+        constexpr auto poly = make_poly(coeffs, powers);
+        REQUIRE(poly.coeffs()[0] == 1.0);
+    }
+
+    SUBCASE("Single-variable polynomial, powers are sorted, no duplicates")
+    {
+        constexpr auto powers = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+        constexpr std::array<double, 4> coeffs{1.0, 2.0, 3.0, 4.0};
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.coeffs()[0] == 1.0);
+        REQUIRE(poly.coeffs()[1] == 2.0);
+        REQUIRE(poly.coeffs()[2] == 3.0);
+        REQUIRE(poly.coeffs()[3] == 4.0);
+    }
+
+    SUBCASE("Single-variable polynomial, powers are not sorted, no duplicates")
+    {
+        constexpr auto powers = PowersList<Powers<0>, Powers<3>, Powers<2>, Powers<1>>{};
+        constexpr std::array<double, 4> coeffs{1.0, 2.0, 3.0, 4.0};
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.coeffs()[0] == 1.0);
+        REQUIRE(poly.coeffs()[1] == 4.0);
+        REQUIRE(poly.coeffs()[2] == 3.0);
+        REQUIRE(poly.coeffs()[3] == 2.0);
+    }
+
+    SUBCASE("Single-variable polynomial, unsorted and duplicate powers")
+    {
+        constexpr auto powers = PowersList<
+            Powers<3>, Powers<4>, Powers<4>, Powers<2>, Powers<1>, Powers<3>, Powers<2>, Powers<0>,
+            Powers<1>>{};
+        constexpr std::array<int, powers.size> coeffs{3, 4, 5, 4, 3, 5, 3, 5, 3};
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.num_terms == 5);
+
+        REQUIRE(poly.coeffs()[0] == 5);
+        REQUIRE(poly.coeffs()[1] == 6);
+        REQUIRE(poly.coeffs()[2] == 7);
+        REQUIRE(poly.coeffs()[3] == 8);
+        REQUIRE(poly.coeffs()[4] == 9);
+    }
+
+    SUBCASE("Multiple variables, unsorted and duplicate powers")
+    {
+        constexpr auto powers = PowersList<
+            Powers<1, 0>, Powers<0, 0>, Powers<1, 0>, Powers<0, 1>, Powers<1, 1>, Powers<0, 1>>{};
+        constexpr std::array<int, powers.size> coeffs{2, 3, 4, 5, 6, 7};
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.num_terms == 4);
+
+        REQUIRE(poly.coeffs()[0] == 3);
+        REQUIRE(poly.coeffs()[1] == 12);
+        REQUIRE(poly.coeffs()[2] == 6);
+        REQUIRE(poly.coeffs()[3] == 6);
+    }
+}
+
+TEST_CASE("Construct a polynomial with a coefficient tuple")
+{
+    SUBCASE("Single-variable polynomial, powers are sorted, no duplicates")
+    {
+        constexpr auto powers = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+        constexpr auto coeffs = std::tuple(1, 2, 3, '\4');
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(std::is_same_v<
+                std::remove_cv_t<decltype(poly)>,
+                Polynomials::Polynomial<int, Powers<0>, Powers<1>, Powers<2>, Powers<3>>>);
+
+        REQUIRE(poly.coeffs()[0] == 1);
+        REQUIRE(poly.coeffs()[1] == 2);
+        REQUIRE(poly.coeffs()[2] == 3);
+        REQUIRE(poly.coeffs()[3] == 4);
+    }
+
+    SUBCASE("Single-variable polynomial, powers are not sorted, no duplicates")
+    {
+        constexpr auto powers = PowersList<Powers<0>, Powers<3>, Powers<2>, Powers<1>>{};
+        constexpr auto coeffs = std::tuple(1.0, 2.0, 3.0, 4.0);
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.coeffs()[0] == 1.0);
+        REQUIRE(poly.coeffs()[1] == 4.0);
+        REQUIRE(poly.coeffs()[2] == 3.0);
+        REQUIRE(poly.coeffs()[3] == 2.0);
+    }
+
+    SUBCASE("Single-variable polynomial, unsorted and duplicate powers")
+    {
+        constexpr auto powers = PowersList<
+            Powers<3>, Powers<4>, Powers<4>, Powers<2>, Powers<1>, Powers<3>, Powers<2>, Powers<0>,
+            Powers<1>>{};
+        constexpr auto coeffs = std::tuple(3, 4, 5, 4, 3, 5, 3, 5, 3);
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.num_terms == 5);
+
+        REQUIRE(poly.coeffs()[0] == 5);
+        REQUIRE(poly.coeffs()[1] == 6);
+        REQUIRE(poly.coeffs()[2] == 7);
+        REQUIRE(poly.coeffs()[3] == 8);
+        REQUIRE(poly.coeffs()[4] == 9);
+    }
+
+    SUBCASE("Multiple variables, unsorted and duplicate powers")
+    {
+        constexpr auto powers = PowersList<
+            Powers<1, 0>, Powers<0, 0>, Powers<1, 0>, Powers<0, 1>, Powers<1, 1>, Powers<0, 1>>{};
+        constexpr auto coeffs = std::tuple(2, 3, 4, 5, 6, 7);
+        constexpr auto poly = make_poly(coeffs, powers);
+
+        REQUIRE(poly.num_terms == 4);
+
+        REQUIRE(poly.coeffs()[0] == 3);
+        REQUIRE(poly.coeffs()[1] == 12);
+        REQUIRE(poly.coeffs()[2] == 6);
+        REQUIRE(poly.coeffs()[3] == 6);
+    }
+}
+
+TEST_CASE("Evaluating a multivariable polynomial")
+{
+    constexpr auto powers = PowersList<Powers<3, 0>, Powers<0, 3>, Powers<2, 1>, Powers<1, 2>,
+        Powers<2, 0>, Powers<0, 2>, Powers<1, 1>, Powers<1, 0>, Powers<0, 1>, Powers<0, 0>>{};
+
+    constexpr std::array<int, powers.size> coeffs { 2, 1, -3, 4, 0, 5, 1, 1, 2, 1 };
+
+    constexpr auto poly = make_poly(coeffs, powers);
+
+    REQUIRE(poly(1, 2) == 48);
+    REQUIRE(poly(2, 2) == 63);
+    REQUIRE(poly(4.0, '\2') == 141);
+}
+
+TEST_CASE("Squaring a single-variable polynomial")
+{
+    constexpr auto powers = PowersList<Powers<2>, Powers<1>, Powers<0>>{};
+    constexpr auto coeffs = std::tuple(1, -1, 2);
+    constexpr auto poly = make_poly(coeffs, powers);
+    constexpr auto poly2 = poly * poly;
+
+    static_assert(std::is_same_v<
+        std::decay_t<decltype(poly2)>,
+        Polynomials::Polynomial<int, Powers<0>, Powers<1>, Powers<2>, Powers<3>, Powers<4>>>);
+
+    REQUIRE(poly2.coeffs()[0] == 4);
+    REQUIRE(poly2.coeffs()[1] == -4);
+    REQUIRE(poly2.coeffs()[2] == 5);
+    REQUIRE(poly2.coeffs()[3] == -2);
+    REQUIRE(poly2.coeffs()[4] == 1);
+}
+
+TEST_CASE("A more complicated, multi-variable test case")
+{
+    constexpr auto powers = PowersList<Powers<0, 0>, Powers<0, 1>, Powers<1, 0>, Powers<1, 1>>{};
+    constexpr auto coeffs1 = std::array<int, 4>{ 2, 1, 2, 3 };
+    constexpr auto coeffs2 = std::array<int, 4>{ 4, 3, 2, 1 };
+    constexpr auto poly1 = make_poly(coeffs1, powers);
+    constexpr auto poly2 = make_poly(coeffs2, powers);
+    constexpr auto result = poly1 * poly2;
+
+    static_assert(result.num_terms == 9);
+    REQUIRE(result.coeffs()[0] == 8);
+    REQUIRE(result.coeffs()[1] == 10);
+    REQUIRE(result.coeffs()[2] == 3);
+    REQUIRE(result.coeffs()[3] == 12);
+    REQUIRE(result.coeffs()[4] == 22);
+    REQUIRE(result.coeffs()[5] == 10);
+    REQUIRE(result.coeffs()[6] == 4);
+    REQUIRE(result.coeffs()[7] == 8);
+    REQUIRE(result.coeffs()[8] == 3);
+}
+
+TEST_CASE("Scalar multiplication test")
+{
+    constexpr auto powers = PowersList<Powers<0, 0>, Powers<0, 1>, Powers<1, 0>, Powers<1, 1>>{};
+    constexpr auto coeffs1 = std::array<int, 4>{2, 1, 2, 3};
+    constexpr auto poly1 = make_poly(coeffs1, powers);
+    auto poly2 = poly1 * 1.5;
+
+    REQUIRE(poly2.coeffs()[0] == 3);
+    REQUIRE(poly2.coeffs()[1] == 1.5);
+    REQUIRE(poly2.coeffs()[2] == 3);
+    REQUIRE(poly2.coeffs()[3] == 4.5);
+
+    auto poly3 = poly1;
+    poly3 *= 1.5;
+    for (int i = 0; i < 4; ++i)
+    {
+        REQUIRE(poly3.coeffs()[i] == static_cast<int>(1.5) * poly3.coeffs()[i]);
+    }
+}
+
+TEST_CASE("Partial derivatives of a single-variable function")
+{
+    constexpr auto powers = PowersList<Powers<0>, Powers<1>, Powers<2>, Powers<3>>{};
+    constexpr auto coefficients = std::array{1.0, 2.0, 3.0, 4.0};
+    constexpr auto poly = make_poly(coefficients, powers);
+    constexpr auto polyprime = partial<0>(poly);
+
+    static_assert(
+        std::is_same_v<
+            std::remove_cv_t<decltype(polyprime)>, Polynomial<double, Powers<0>, Powers<1>, Powers<2>>>);
+
+    REQUIRE(polyprime.coeffs()[0] == 2.0);
+    REQUIRE(polyprime.coeffs()[1] == 6.0);
+    REQUIRE(polyprime.coeffs()[2] == 12.0);
+}
+
+TEST_CASE("Partial derivatives for a function of two variables")
+{
+    constexpr auto powers = PowersList<
+        Powers<0, 0>, Powers<0, 1>, Powers<0, 2>, Powers<1, 0>, Powers<1, 1>, Powers<1, 2>, Powers<2, 2>>{};
+    constexpr int coefficients[] = {1, 2, 1, 2, 1, 2, 1};
+    constexpr auto poly = make_poly(coefficients, powers);
+    constexpr auto d0poly = partial<0>(poly);
+
+    using T = decltype(std::declval<int>() * std::declval<unsigned>());
+    static_assert(std::is_same_v<
+        std::remove_cv_t<decltype(d0poly)>,
+        Polynomial<T, Powers<0, 0>, Powers<0, 1>, Powers<0, 2>, Powers<1, 2>>>);
+
+    REQUIRE(d0poly.coeffs()[0] == 2);
+    REQUIRE(d0poly.coeffs()[1] == 1);
+    REQUIRE(d0poly.coeffs()[2] == 2);
+    REQUIRE(d0poly.coeffs()[3] == 2);
+
+    constexpr auto d1poly = partial<1>(poly);
+    static_assert(std::is_same_v<
+        std::remove_cv_t<decltype(d1poly)>,
+        Polynomial<T, Powers<0, 0>, Powers<0, 1>, Powers<1, 0>, Powers<1, 1>, Powers<2, 1>>>);
+    REQUIRE(d1poly.coeffs()[0] == 2);
+    REQUIRE(d1poly.coeffs()[1] == 2);
+    REQUIRE(d1poly.coeffs()[2] == 1);
+    REQUIRE(d1poly.coeffs()[3] == 4);
+    REQUIRE(d1poly.coeffs()[4] == 2);
+}
+
+#endif // DOCTEST_LIBRARY_INCLUDED
+
 } // namespace Polynomials
 
 } // namespace Galerkin
